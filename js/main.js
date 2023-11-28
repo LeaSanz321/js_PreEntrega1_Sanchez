e469684430f848861504c8229cf7f394714b4e99
Leandro Sánchez <leandroukf321@gmail.com>
2023-11-28 06:51:56 -0300

    backup

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function numeroAlAzar(){
     return Math.ceil(Math.random()*10);
 }
+
 function validarDato (dato){
     if (dato != "" && dato != isNaN){
         return true;
@@ -10,44 +11,44 @@ function validarDato (dato){
     }
 }
 
+function preguntar(){
+    let pregunta = prompt("¿Quieres jugar una vez más? (si o no)").toLowerCase();
 
-let numeroGanador = numeroAlAzar();
-
-let intentos = 0;
-
-let ganador = false;
-
-while(ganador == false){
-   
-    let intentosMaximos = prompt("Seleccione USTED -> en cuántos intentos cree que puede adivinar?");
-    let intentoUsuario = prompt("Intente adivinar el número del 1 al 10:");
-
-    for (let i = 0; i < intentosMaximos; i++){
-        if (validarDato(intentoUsuario)){
-            intentos ++;
-            
-            let numeroUsuario = Number(intentoUsuario);
-    
-            if (numeroUsuario === numeroGanador){
-                alert("Número: " + numeroGanador);
-                alert("¡FELICIDADES! Haz acertado con " + intentos + " intentos");
-                ganador = true;
-            }else{
-                alert("Ups! Lo siento, número incorrecto!");
-                continue;
-            }
-            
-        }else{
-            alert("Por favor ingresa un número váido.")
-        }
-    };
-
-
+    switch(pregunta){
+        case "no":
+            break;
+        case "si": 
+            inicio();
+    }
 }
 
+function inicio (){
+    let intentosMaximos = prompt("Seleccione USTED -> en cuántos intentos cree que puede adivinar?");
 
-
-
-
-
-
+    for (let i = 0; i < intentosMaximos; i++){
+        let ganador = false;
+        while(ganador == false){
+            let numeroGanador = numeroAlAzar();
+            let intentoUsuario = prompt("Intente adivinar el número del 1 al 10:");
+            if (validarDato(intentoUsuario) == true){
+                intentos ++;
+                
+                let numeroUsuario = Number(intentoUsuario);
+        
+                if (numeroUsuario === numeroGanador){
+                    alert("Número: " + numeroGanador);
+                    alert("¡FELICIDADES! Haz acertado con " + i + " intentos");
+                    ganador = true;
+                }else{
+                    alert("Ups! Lo siento, número incorrecto!");
+                    break;
+                }
+            }else{
+                alert("Por favor ingresa un número váido.")
+                break;
+            }
+        };
+    }
+    preguntar();
+}
+inicio();
